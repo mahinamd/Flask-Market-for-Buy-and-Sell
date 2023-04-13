fb123b798b8832c214172458ccfd62e5cd9bdb88
Mahin Ahmed <iahmed.mahin@gmail.com>
2023-04-14 04:28:53 +0600

    first commit

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ app.config["DEBUG"] = True
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 item = mydb["item"]
-owned = mydb["owned"]
 myregis = mydb["myregi"]
 app.config['SECRET_KEY'] = 'Thisisasecret'
 
@@ -28,9 +27,8 @@ def registration():
         d["email"] = request.form["email"]
         d["password1"]=request.form["password1"]
         d["password2"] = request.form["password2"]
-        d["budget"]=1500
         
-
+        
 
         if d["password1"]==d["password2"] and re.fullmatch(regex,d["email"]):
             successMsg="Registration Successful"
@@ -46,7 +44,8 @@ def registration():
 
 
     return render_template("registration.html" , **locals())
-######LOGIN ##########################
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -73,6 +72,8 @@ def login():
                 return render_template("login.html", s=s)
 
     return render_template("login.html", s=s)
+
+
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     logoutMsg = "You are logged out"
@@ -80,18 +81,18 @@ def logout():
     session.pop("name",None)
     print(session)
     return render_template('login.html',s=logoutMsg) 
+
+
 @app.route('/item',methods=['GET', 'POST'])
 def item_page():
     user = myregis.find_one({"name":session['name']})
     current_user=session["name"]
     if request.method =="POST":
         d=dict()
-        d["id"] = request.form["id"]
-        d["name2"]= request.form["name"]
-        d["name"]=session["name"]
-        
-        d["price"]=request.form["price"]
-        d["barcode"] = request.form["barcode"]
+       
+        d["name"] = request.form["name"]
+        d["email"] = request.form["email"]
+        d["type"] = request.form["type"]
         d["description"] = request.form["description"]
         
         item.insert_one(d)
@@ -110,104 +111,6 @@ def homepage():
         current_user='Unknown'
         return render_template("home.html",**locals())
 
-    
-   
-    
-@app.route('/purchase',methods=['GET', 'POST'])
-def purchase():
-    if "name" in session:
-        current_user=session["name"]
-        if request.method == "POST":
-            name2 = request.form["purchased_item"]
-            price = 0
-            budget=0
-            budget2=0
-            name=""
-            barcode=""
-            description=""
-            j=0
-            k=0
-            d=dict()
-            for i in item.find({"name2":name2}):
-                price = i["price"]
-                name2 = i["name2"]
-                name = i["name"]
-                barcode = i["barcode"]
-                description = i["description"]
-                price = int(price)
-                
-            for i in myregis.find({"name":session["name"]}):
-                budget = int(i["budget"])
-                print(type(budget))
-                j=budget-price
-            myregis.update_one({"name":session["name"]},{"$set":{"budget":j}})
-            d["name"]=session["name"]
-            d["price"]=price
-            d["name2"]=name2
-            d["barcode"]=barcode
-            d["description"]=description
-
-            owned.insert_one(d)
-            item.delete_one({"name2":name2})
-            for i in myregis.find({"name":name}):
-                budget2 = int(i["budget"])
-                k=budget2+price
-            myregis.update_one({"name":name},{"$set":{"budget":k}})
-
-
-            print(j)
-    else:
-        return redirect('/login')
-
-    return redirect('/market')
-
-@app.route('/sell',methods=['GET', 'POST'])
-def sell():
-    if "name" in session:
-        current_user=session["name"]
-        if request.method == "POST":
-            name2 = request.form["sold_item"]
-            price = 0
-            budget=0
-            barcode=""
-            description=""
-            j=0
-            d=dict()
-            for i in owned.find({"name2":name2}):
-                price = i["price"]
-                name2 = i["name2"]
-                barcode = i["barcode"]
-                description = i["description"]
-                price = int(price)
-                
-            for i in myregis.find({"name":session["name"]}):
-                budget = int(i["budget"])
-                print(type(budget))
-                j=budget+price
-            #myregis.update_one({"name":session["name"]},{"$set":{"budget":j}})
-            d["name"]=session["name"]
-            d["price"]=price
-            d["name2"]=name2
-            d["barcode"]=barcode
-            d["description"]=description
-            
-            item.insert_one(d)
-            owned.delete_one({"name2":name2})
-    else:
-        return redirect('/login')
-    return redirect('/market')
-
-@app.route('/market')
-def market():
-    if "name" in session:
-
-        user = myregis.find_one({"name":session['name']})
-        current_user=session["name"]
-        return render_template('market.html',items=item.find(),owns=owned.find(),**locals())
-    else:
-        user='Unknown'
-        current_user='Unknown'
-        return render_template('market.html',items=item.find(),owns=owned.find(),**locals())
 
 @app.route('/profile')
 def profile():
